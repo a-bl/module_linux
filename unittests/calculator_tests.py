722dc20da272f8fe0fc5898e4e00dc8abdc544ed
a-bl <blyzniuk.anastasiia@gmail.com>
2021-11-30 09:31:12 +0200

    Add skipping block to file

--- unittests/calculator_tests.py
+++ unittests/calculator_tests.py
@@ -4,16 +4,18 @@ from calculator import Calculator
 
 class CalculatorTests(unittest.TestCase):
 
+    def setUp(self):
+        self.calc = Calculator()
     def test_add(self):
         '''Test case function for addition'''
-        self.calc = Calculator()
+        #self.calc = Calculator()
         result = self.calc.add(4, 7)
         expected = 11
         self.assertEqual(result, expected)
 
     def test_subtract(self):
         '''Test case function for subtraction'''
-        self.calc = Calculator()
+        #self.calc = Calculator()
         result = self.calc.subtract(10, 5)
         expected = 5
         self.assertEqual(result, expected)
@@ -21,19 +23,19 @@ class CalculatorTests(unittest.TestCase):
 
     def test_multiply(self):
         '''Test case function for multiplication'''
-        self.calc = Calculator()
+        #self.calc = Calculator()
         result = self.calc.multiply(3, 7)
         expected = 21
         self.assertEqual(result, expected)
 
     def test_divide(self):
         '''Test case function for division'''
-        self.calc = Calculator()
+        #self.calc = Calculator()
         result = self.calc.divide(10, 2)
         expected = 5
         self.assertEqual(result, expected)
 
-    @unittest.skip('Some reason')
+    @unittest.skip('implemented try/except block')
     def test_div_error(self):
         '''Make sure ZeroDivisionError is raised when necessary'''
         self.calc = Calculator()
