7253a76e2661de6c21355c78b81d6b78f851df8e
anastasiia <blyzniuk.anastasiia@gmail.com>
2021-12-19 19:31:57 +0200

    Add Dockerfile

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -5,3 +5,6 @@ from app.models import User, Post, Interview, Question, Grade
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post, 'Interview': Interview, 'Question': Question, 'Grade': Grade}
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000)
