8338e6861c9bed2554ae7b6a77a8f1a2568d9611
anastasiia <blyzniuk.anastasiia@gmail.com>
2021-12-20 16:02:24 +0200

    Remove unnecessary files

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -1,11 +1,4 @@
-from app import app, db
-from app.models import User, Interview, Question, Grade
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Interview': Interview, 'Question': Question, 'Grade': Grade}
-
+from app import app
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 5000)
